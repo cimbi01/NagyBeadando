fa1a40f0c5f9472a205694d183e9f59354802061
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-04-14 21:08:06 +0200

    ITermelő hozzáadva

---
+++ NagyBeadandó/Mezok/Alapok/ITermelo.cs
@@ -0,0 +1,14 @@
+﻿namespace NagyBeadandó.Mezok.Alapok
+{
+    internal interface ITermelo
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Termelést indít a termelő osztályon
+        /// </summary>
+        void Termel();
+
+        #endregion Public Methods
+    }
+}
