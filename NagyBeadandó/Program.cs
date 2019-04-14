a679a2b873fdbcae1a728a0bbf89ba70683cbff5
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-04-14 20:18:27 +0200

    Diagramok, ppt Hozzáadva

--- NagyBeadandó/Program.cs
+++ NagyBeadandó/Program.cs
@@ -1,21 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NagyBeadandó
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+        #region Private Methods
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        private static void Main(string[] args)
+        {
+            Console.ReadKey();
         }
+
+        #endregion Private Methods
     }
 }
