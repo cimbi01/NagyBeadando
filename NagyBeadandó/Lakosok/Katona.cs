9f6816feca1b869c3a05d6bb9093330505681fde
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-04-15 18:25:04 +0200

    Katona Hozzáadva Lakos property-k set > protected-re állítva

---
+++ NagyBeadandó/Lakosok/Katona.cs
@@ -0,0 +1,31 @@
+﻿using NagyBeadandó.Mezok.Alapok;
+
+namespace NagyBeadandó.Lakosok
+{
+    internal class Katona : Lakos
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// KatonaTípus alapján beállít mindent
+        /// </summary>
+        /// <param name="katona_tipus">Katona Típusa</param>
+        public Katona(int id, Tipusok.KatonaTipusok katona_tipus)
+        {
+            ID = id;
+            KatonaTipus = katona_tipus;
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public bool ItthonVan { get; private set; } = true;
+        public Tipusok.KatonaTipusok KatonaTipus { get; private set; }
+        public int MenetSebesseg { get; private set; }
+        public int RomboloErtek { get; private set; }
+        public int TamadoErtek { get; private set; }
+
+        #endregion Public Properties
+    }
+}
--- NagyBeadandó/Lakosok/Lakos.cs
+++ NagyBeadandó/Lakosok/Lakos.cs
@@ -4,14 +4,18 @@
     {
         #region Public Properties
 
+        public Lakos()
+        {
+        }
         public Lakos(int id, int _Fogyasztas)
         {
             Fogyasztas = _Fogyasztas;
             ID = id;
         }
-        public int Fogyasztas { get; private set; }
-        public int ID { get; private set; }
+        public int Fogyasztas { get; protected set; }
+        public int ID { get; protected set; }
         public bool MegVanEtetve { get; set; } = false;
+        public int VedoErtek { get; protected set; } = 1;
 
         #endregion Public Properties
     }
