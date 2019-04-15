1c5a3a77f96bfdc78f7f764fe9638bbea2dd6bf8
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-04-15 17:56:48 +0200

    FoEpulet hozzáadva FőéületTest hozzáadva

---
+++ NagyBeadandó/Mezok/FoEpulet.cs
@@ -0,0 +1,70 @@
+﻿using NagyBeadandó.Mezok.Alapok;
+using System;
+using System.Collections.Generic;
+
+namespace NagyBeadandó.Mezok
+{
+    public class FoEpulet : Tarolo, ITaroloTermelo
+    {
+        #region Public Constructors
+
+        public FoEpulet(int id, Tipusok.MezoTipusok mezotipus, Dictionary<Tipusok.Tarolhatok, int[]> kapacitas) : base(id, mezotipus, kapacitas)
+        {
+            InteraktivMezo = new InteraktivFoEpulet(this);
+        }
+        public FoEpulet(FoEpulet fep) : base(fep.ID, fep.MezoTipus, fep.Kapacitas)
+        { }
+
+        #endregion Public Constructors
+
+        #region Private Fields
+
+        private readonly int termeles = 10;
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        /// <summary>
+        /// termeles-nyit termel pluszban a meglevohoz a tarolhato-bol
+        /// </summary>
+        /// <param name="tarolhato">A tarolhato tipusa, amibol termel</param>
+        public void Termel(Tipusok.Tarolhatok tarolhato)
+        {
+            // növeli a tarolt mennyiseget
+            Kapacitas[tarolhato][0] += this.termeles;
+            // ha a taroltmennyiseg > mint a maxkapactitás, akkor egyenlővé teszi vele
+            if (Kapacitas[tarolhato][0] > Kapacitas[tarolhato][1])
+            {
+                Kapacitas[tarolhato][0] = Kapacitas[tarolhato][1];
+            }
+        }
+
+        #endregion Public Methods
+
+        #region Private Classes
+
+        private class InteraktivFoEpulet : FoEpulet, IInteraktivMezo
+        {
+            #region Public Constructors
+
+            public InteraktivFoEpulet(FoEpulet fep) : base(fep)
+            {
+                InteraktivMezo = this;
+                VanBennePublikusMetodus = Metodusok.Count == 0;
+            }
+
+            #endregion Public Constructors
+
+            #region Public Properties
+
+            public Dictionary<string, Action> Metodusok { get; private set; } = new Dictionary<string, Action>();
+
+            public bool VanBennePublikusMetodus { get; private set; }
+
+            #endregion Public Properties
+        }
+
+        #endregion Private Classes
+    }
+}
---
+++ NagyBeadandóTests/Mezok/FoEpuletTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NagyBeadandó.Mezok.Alapok;
+using System.Collections.Generic;
+
+namespace NagyBeadandó.Mezok.Tests
+{
+    [TestClass()]
+    public class FoEpuletTests
+    {
+        #region Public Constructors
+
+#pragma warning disable S3963 // "static" fields should be initialized inline
+        static FoEpuletTests()
+        {
+            kapacitas.Add(Tipusok.Tarolhatok.Katona, kapacitasok);
+            fepulet = new FoEpulet(id, mezotipus, kapacitas);
+        }
+#pragma warning restore S3963 // "static" fields should be initialized inline
+
+        #endregion Public Constructors
+
+        #region Private Fields
+
+        private static readonly int id = 0;
+        private static readonly Dictionary<Tipusok.Tarolhatok, int[]> kapacitas = new Dictionary<Tipusok.Tarolhatok, int[]>();
+        private static readonly int[] kapacitasok = new int[] { 0, 1 };
+        private static readonly Tipusok.MezoTipusok mezotipus = Tipusok.MezoTipusok.Kaszarnya;
+        private static readonly FoEpulet fepulet;
+
+        #endregion Private Fields
+
+        [TestMethod()]
+        public void TermelTest()
+        {
+            foreach (Tipusok.Tarolhatok item in fepulet.Kapacitas.Keys)
+            {
+                Assert.AreEqual(false, fepulet.MegVanTelve(item));
+                fepulet.Termel(item);
+                Assert.AreEqual(true, fepulet.MegVanTelve(item));
+            }
+        }
+    }
+}
