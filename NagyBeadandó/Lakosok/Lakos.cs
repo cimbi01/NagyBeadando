653c85113d2c808e59c664b2e7fb262cd3633c35
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-04-15 18:16:55 +0200

    Lakos hozzáadva

---
+++ NagyBeadandó/Lakosok/Lakos.cs
@@ -0,0 +1,18 @@
+﻿namespace NagyBeadandó.Lakosok
+{
+    public class Lakos
+    {
+        #region Public Properties
+
+        public Lakos(int id, int _Fogyasztas)
+        {
+            Fogyasztas = _Fogyasztas;
+            ID = id;
+        }
+        public int Fogyasztas { get; private set; }
+        public int ID { get; private set; }
+        public bool MegVanEtetve { get; set; } = false;
+
+        #endregion Public Properties
+    }
+}
