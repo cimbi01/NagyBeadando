122f6c643de33931b68d00d254933065b81767e4
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-04-20 14:48:15 +0200

    Lakos ID, Katona törölve Fogyasztas, MenetSebesseg, TamadoErtek, VedoErtek random KatonaiEgysegTest törölve

--- NagyBeadandó/Lakosok/Lakos.cs
+++ NagyBeadandó/Lakosok/Lakos.cs
@@ -1,33 +1,32 @@
-﻿namespace NagyBeadandó.Lakosok
+﻿using System;
+
+namespace NagyBeadandó.Lakosok
 {
     public class Lakos
     {
-        #region Private Fields
-
-        private static int CurrentId = 0;
-
-        #endregion Private Fields
-
         #region Public Properties
 
-        public Lakos(int _Fogyasztas = 1)
+        public Lakos()
         {
-            Fogyasztas = _Fogyasztas;
-            ID = CurrentId++;
+            Random rnd = new Random();
+            Fogyasztas = rnd.Next(1, 10);
+            MenetSebesseg = rnd.Next(1, 10);
+            TamadoErtek = rnd.Next(1, 10);
+            VedoErtek = rnd.Next(1, 10);
         }
         public Lakos(Lakos lakos)
         {
             Fogyasztas = lakos.Fogyasztas;
-            ID = lakos.ID;
+            MenetSebesseg = lakos.MenetSebesseg;
+            TamadoErtek = lakos.TamadoErtek;
+            VedoErtek = lakos.VedoErtek;
         }
         public int Fogyasztas { get; protected set; }
-        public int ID { get; protected set; }
         public bool ItthonVan { get; set; } = true;
-        public bool Katona { get; protected set; } = false;
         public bool MegVanEtetve { get; set; } = false;
-        public int MenetSebesseg { get; private set; } = 5;
-        public int TamadoErtek { get; private set; } = 1;
-        public int VedoErtek { get; protected set; } = 1;
+        public int MenetSebesseg { get; private set; }
+        public int TamadoErtek { get; private set; }
+        public int VedoErtek { get; protected set; }
 
         #endregion Public Properties
     }
