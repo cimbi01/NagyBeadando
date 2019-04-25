5466ebaf55ca1eb0226f7e97ffab1d4196bdd787
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-04-25 19:09:42 +0200

    Gondolatok folytatva TipusTarolo hozzaadva Tarolo kommentezve

--- NagyBeadandó/Mezok/Alapok/Tarolo/Tarolo.cs
+++ NagyBeadandó/Mezok/Alapok/Tarolo/Tarolo.cs
@@ -8,11 +8,10 @@ namespace NagyBeadandó.Mezok.Alapok.Tarolo
         #region Public Methods
 
         /// <summary>
-        /// A Kapacitás tároló részéhez [0] hozzáad mennyit-nyit
-        /// Ha így nagyobb akkor teletölti, majd dob egy TaroloTulCsordultException-t
+        /// A tarolt tuple mennyiseg részét növeli mennyit-el
+        /// Ha így a mennyiseg tullepi a kapacitast, dob egy TaroloTulCsordultException-t
         /// </summary>
-        /// <param name="tarolhato">A tárolható típusa, amelyik rekeszbe szeretnék tenni a mennyit</param>
-        /// <param name="mennyit">A mennyiség, amennyit be szeretnénk tenni a rekeszbe</param>
+        /// <param name="mennyit">mennyiseg, amennyit bele szeretne tolteni</param>
         public void Betesz(int mennyit)
         {
             int tarolo_maradekhely = this.tarolt.kapacitas - this.tarolt.mennyiseg;
@@ -23,12 +22,12 @@ namespace NagyBeadandó.Mezok.Alapok.Tarolo
             }
             this.tarolt.mennyiseg += tarolo_maradekhely;
         }
+
         /// <summary>
-        /// Visszaadja mmenyit-nyit a Tárolhato-ból
+        /// Tarolt tuple mennyiseg-ből elvesz mennyit
+        /// Ha a mennyit nagyobb mint a tarolt mennyiseg, akkor NincsElegTarolhatoException-t dob
         /// </summary>
-        /// <param name="tarolhato">A típus, amiből lekérjük a mennyiséget</param>
-        /// <param name="mennyit">A mennyiség, hogy mennyit szeretne kivenni a tárolóból</param>
-        /// <returns>Visszaad mennyit-nyit, ha van belőle mennyit-nyi a kapacitásban, ellenkező esetben dob egy NincsElégTarolhatoException-t</returns>
+        /// <param name="mennyit">Mennyiseg, amennyit ki szeretne venni</param>
         public void Kivesz(int mennyit)
         {
             if (this.tarolt.mennyiseg < mennyit)
@@ -38,6 +37,9 @@ namespace NagyBeadandó.Mezok.Alapok.Tarolo
             this.tarolt.mennyiseg -= mennyit;
         }
 
+        /// </summary>
+        /// <param name="tarolhato">Tarolhato tipusa</param>
+        /// <returns>Visszaadja, hogy a tarolhato egyenlő-e a tarolt tuple tarolt_tipus-al</returns>
         public bool VanBenne(Tipusok.Tarolhatok tarolhato)
         {
             return this.tarolt.tarolt_tipus == tarolhato;
@@ -47,12 +49,22 @@ namespace NagyBeadandó.Mezok.Alapok.Tarolo
 
         #region Private Fields
 
+        /// <summary>
+        /// Tarolt tuple
+        /// tarolt_tipus a tarolhato, amit tarol a tuple
+        /// mennyiseg a mennyiseg, amennyit tarol a tuple
+        /// kapacitas a kapacitas, amennyi a maximum mennyiseg lehet
+        /// </summary>
         private (Tipusok.Tarolhatok tarolt_tipus, int mennyiseg, int kapacitas) tarolt;
 
         #endregion Private Fields
 
         #region Public Constructors
 
+        /// <summary>
+        /// Inicializálja a tárolót
+        /// </summary>
+        /// <param name="tarolhato">Tarolhato tipusa, amiből a tuple-t inicializálja</param>
         public Tarolo(Tipusok.Tarolhatok tarolhato)
         {
             this.tarolt = ValueTuple.Create(tarolhato, 0, 0);
---
+++ NagyBeadandó/Mezok/Alapok/Tarolo/TipusTarolo.cs
@@ -0,0 +1,83 @@
+﻿using NagyBeadandó.Kivételek.MezoKivetelek;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NagyBeadandó.Mezok.Alapok.Tarolo
+{
+    internal class TipusTarolo<T>
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Hozzáaadja a paraméterként kapott listát a tarolt tuple tarolt_tarolhatok-jához
+        /// Ha a tarolt menniseg és a paraméter össze hossza nagyobb mint a kapacitas
+        /// Akkor feltölti majd dob egy TaroloTulCsordultException-t
+        /// </summary>
+        /// <param name="tarolhatok">Lista, amit hozzaadunk</param>
+        public void Betesz(List<T> tarolhatok)
+        {
+            for (int i = 0; i < tarolhatok.Count && this.tarolt.tarolt_tarolhatok.Count <= this.tarolt.kapacitas; i++)
+            {
+                this.tarolt.tarolt_tarolhatok.Add(tarolhatok[i]);
+                tarolhatok.Remove(tarolhatok[i]);
+                i--;
+            }
+            if (tarolhatok.Count > 0)
+            {
+                throw new TaroloTulCsordultException();
+            }
+        }
+        /// <summary>
+        /// A tarolt tuple mennyiseg részét növeli mennyit-el
+        /// Ha így a mennyiseg tullepi a kapacitast, dob egy TaroloTulCsordultException-t
+        /// </summary>
+        /// <param name="mennyit">mennyiseg, amennyit bele szeretne tolteni</param>
+        public void Betesz(T tarolhato)
+        {
+            int tarolo_maradekhely = this.tarolt.kapacitas - this.tarolt.tarolt_tarolhatok.Count;
+            if (1 > tarolo_maradekhely)
+            {
+                throw new TaroloTulCsordultException();
+            }
+            this.tarolt.tarolt_tarolhatok.Add(tarolhato);
+        }
+        /// <summary>
+        /// Tarolt tuple mennyiseg-ből elvesz mennyit
+        /// Ha a mennyit nagyobb mint a tarolt mennyiseg, akkor NincsElegTarolhatoException-t dob
+        /// </summary>
+        /// <param name="mennyit">Mennyiseg, amennyit ki szeretne venni</param>
+        /// <param name="predicate">A lamdbda kifejezes, amivel szuri a listából az elemeket</param>
+        public List<T> Kivesz(Predicate<T> predicate)
+        {
+            Func<T, bool> kivalasztas = new Func<T, bool>(predicate);
+            IEnumerable<T> predicate_lista = this.tarolt.tarolt_tarolhatok.Where(kivalasztas);
+            this.tarolt.tarolt_tarolhatok.RemoveAll(predicate);
+            return predicate_lista.ToList();
+        }
+        /// </summary>
+        /// <param name="tarolhato">Tarolhato tipusa</param>
+        /// <returns>Visszaadja, hogy a tarolhato egyenlő-e a tarolt tuple tarolt_tipus-al</returns>
+        public bool VanBenne(Tipusok.Tarolhatok tarolhato)
+        {
+            return this.tarolt.tarolt_tipus == tarolhato;
+        }
+
+        #endregion Public Methods
+
+        #region Private Fields
+
+        private (Tipusok.Tarolhatok tarolt_tipus, List<T> tarolt_tarolhatok, int kapacitas) tarolt;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public TipusTarolo(Tipusok.Tarolhatok tarolhato)
+        {
+            this.tarolt = ValueTuple.Create(tarolhato, new List<T>(), 0);
+        }
+
+        #endregion Public Constructors
+    }
+}
